2a9e8ce6ffc0c32c56ca04c209e6415ff70661cc
Maria <maryika93@mail.ru>
2018-02-10 12:46:34 +0600

    less5

--- less5.php
+++ less5.php
@@ -1,50 +1,7 @@
 <?php
-$telnumbers = [
-    [
-        'firstName' => 'Иван',
-        'lastName' => 'Иванов',
-        'address' => 'г.Москва, ул. Алиева,2',
-        'phoneNumber' => '812 123-1234'
-    ],
-    [
-        'firstName' => 'Максим',
-        'lastName' => 'Максимов',
-        'address' => 'г.Тверь, ул. Омская,2',
-        'phoneNumber' => '123 346-8946'
-    ],
-    [
-        'firstName' => 'Петр',
-        'lastName' => 'Петров',
-        'address' => 'г.Псков, ул. Жукова,2',
-        'phoneNumber' => '321 578-9875'
-    ],
-    [
-        'firstName' => 'Виктор',
-        'lastName' => 'Викторов',
-        'address' => 'г.Уфа, ул. Думская,2',
-        'phoneNumber' => '567 235-1068'
-    ],
-    [
-        'firstName' => 'Мария',
-        'lastName' => 'Марьева',
-        'address' => 'г.Иркутск, ул. Ленина,2',
-        'phoneNumber' => '789 567-8755'
-    ],
-    [
-        'firstName' => 'Сергей',
-        'lastName' => 'Сергеев',
-        'address' => 'г.Киров, ул. Сенная,2',
-        'phoneNumber' => '853 064-4578'
-    ],
-];
-
-$numbers = json_encode($telnumbers,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
 $tels = file_get_contents(__DIR__.'/tel.json');
 $telephones = json_decode($tels,true);
-
-echo '<pre>';
-print_r($telephones);
 
 ?>
 
